70386245ef98fa90367c08c69e530e01b3564d41
Екатерина Никифорова <dayso@192.168.1.17>
2023-03-23 00:25:47 +0300

    toggle switch new

--- src/javascript/antitrendstoggle.js
+++ src/javascript/antitrendstoggle.js
@@ -1,30 +1,71 @@
-const toggleSwitch = document.querySelector('.M_ToggleSwitch')
-const toggleSwitchOption = document.querySelectorAll('.A_ToggleSwitchOption')
-const scrollOption = document.querySelectorAll('.A_ToggleSwitchOption')[0]
-const galleryOption = document.querySelectorAll('.A_ToggleSwitchOption')[1]
 const sectionScroll = document.querySelector('.S_Scroll')
 const sectionGallery = document.querySelector('.S_Gallery')
 
-for (let i = 0; i < toggleSwitchOption.length; i++) {
-  toggleSwitch.addEventListener('click', (e) => {
-    toggleSwitchOption[i].classList.toggle('active')
-
-    if (scrollOption.classList.contains('active')) {
-      sectionScroll.classList.add('active'),
-        sectionGallery.classList.remove('active')
-    } else {
-      sectionGallery.classList.add('active'),
-        sectionScroll.classList.remove('active')
-    }
-  })
-}
-
 const runningLineOne = document.querySelector('.M_RunningLine')
-
 const mediaphone = window.matchMedia(
   '(min-width: 768px) and (max-width: 3000px)'
 )
 
-if (mediaphone.matches) {
-  runningLineOne.innerHTML = runningLineOne.innerHTML + runningLineOne.innerHTML
+let navbarHeight = document.querySelector('.S_NavBar').offsetHeight
+
+let gallery = document.getElementById('gallery')
+let scroll = document.getElementById('scroll')
+let toggleSwitch = document.querySelector('.M_ToggleSwitch')
+
+function toggleSwitchInit() {
+  sectionGallery.style.marginTop = +navbarHeight + 'px'
+
+  gallery.addEventListener('click', () => {
+    toggleSwitch.classList.add('off'),
+      sectionGallery.classList.add('active'),
+      sectionScroll.classList.remove('active')
+  })
+
+  scroll.addEventListener('click', () => {
+    toggleSwitch.classList.remove('off')
+    sectionScroll.classList.add('active'),
+      sectionGallery.classList.remove('active')
+  })
 }
+
+function runningLinesInit() {
+  if (mediaphone.matches) {
+    runningLineOne.innerHTML =
+      runningLineOne.innerHTML + runningLineOne.innerHTML
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  runningLinesInit(), toggleSwitchInit()
+})
+
+// const toggleSwitch = document.querySelector('.M_ToggleSwitch')
+// const toggleSwitchOption = document.querySelectorAll('.A_ToggleSwitchOption')
+// const scrollOption = document.querySelectorAll('.A_ToggleSwitchOption')[0]
+// const galleryOption = document.querySelectorAll('.A_ToggleSwitchOption')[1]
+// const sectionScroll = document.querySelector('.S_Scroll')
+// const sectionGallery = document.querySelector('.S_Gallery')
+//
+// for (let i = 0; i < toggleSwitchOption.length; i++) {
+//   toggleSwitch.addEventListener('click', (e) => {
+//     toggleSwitchOption[i].classList.toggle('active')
+//
+//     if (scrollOption.classList.contains('active')) {
+//       sectionScroll.classList.add('active'),
+//         sectionGallery.classList.remove('active')
+//     } else {
+//       sectionGallery.classList.add('active'),
+//         sectionScroll.classList.remove('active')
+//     }
+//   })
+// }
+//
+// const runningLineOne = document.querySelector('.M_RunningLine')
+//
+// const mediaphone = window.matchMedia(
+//   '(min-width: 768px) and (max-width: 3000px)'
+// )
+//
+// if (mediaphone.matches) {
+//   runningLineOne.innerHTML = runningLineOne.innerHTML + runningLineOne.innerHTML
+// }
--- src/javascript/influencerstoggle.js
+++ src/javascript/influencerstoggle.js
@@ -1,32 +1,76 @@
-const toggleSwitch = document.querySelector('.M_ToggleSwitch')
-const toggleSwitchOption = document.querySelectorAll('.A_ToggleSwitchOption')
-const influencerOption = document.querySelectorAll('.A_ToggleSwitchOption')[0]
-const movieOption = document.querySelectorAll('.A_ToggleSwitchOption')[1]
 const sectionInfluencers = document.querySelector('.S_Influencers')
 const sectionMovies = document.querySelector('.S_Movies')
 
-for (let i = 0; i < toggleSwitchOption.length; i++) {
-  toggleSwitch.addEventListener('click', (e) => {
-    toggleSwitchOption[i].classList.toggle('active')
-
-    if (influencerOption.classList.contains('active')) {
-      sectionInfluencers.classList.add('active'),
-        sectionMovies.classList.remove('active')
-    } else {
-      sectionMovies.classList.add('active'),
-        sectionInfluencers.classList.remove('active')
-    }
-  })
-}
-
 const runningLineOne = document.querySelector('.M_RunningLine')
 const runningLineTwo = document.querySelector('.M_RunningLineTwo')
-
 const mediaphone = window.matchMedia(
   '(min-width: 768px) and (max-width: 3000px)'
 )
 
-if (mediaphone.matches) {
-  runningLineOne.innerHTML = runningLineOne.innerHTML + runningLineOne.innerHTML
-  runningLineTwo.innerHTML = runningLineTwo.innerHTML + runningLineTwo.innerHTML
+let navbarHeight = document.querySelector('.S_NavBar').offsetHeight
+
+let movie = document.getElementById('movie')
+let influencer = document.getElementById('influencer')
+let toggleSwitch = document.querySelector('.M_ToggleSwitch')
+
+function toggleSwitchInit() {
+  sectionMovies.style.marginTop = +navbarHeight + 'px'
+
+  movie.addEventListener('click', () => {
+    toggleSwitch.classList.add('off'),
+      sectionMovies.classList.add('active'),
+      sectionInfluencers.classList.remove('active')
+  })
+
+  influencer.addEventListener('click', () => {
+    toggleSwitch.classList.remove('off')
+    sectionInfluencers.classList.add('active'),
+      sectionMovies.classList.remove('active')
+  })
 }
+
+function runningLinesInit() {
+  if (mediaphone.matches) {
+    runningLineOne.innerHTML =
+      runningLineOne.innerHTML + runningLineOne.innerHTML
+    runningLineTwo.innerHTML =
+      runningLineTwo.innerHTML + runningLineTwo.innerHTML
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  runningLinesInit(), toggleSwitchInit()
+})
+
+// const toggleSwitch = document.querySelector('.M_ToggleSwitch')
+// const toggleSwitchOption = document.querySelectorAll('.A_ToggleSwitchOption')
+// const influencerOption = document.querySelectorAll('.A_ToggleSwitchOption')[0]
+// const movieOption = document.querySelectorAll('.A_ToggleSwitchOption')[1]
+// const sectionInfluencers = document.querySelector('.S_Influencers')
+// const sectionMovies = document.querySelector('.S_Movies')
+//
+// for (let i = 0; i < toggleSwitchOption.length; i++) {
+//   toggleSwitch.addEventListener('click', (e) => {
+//     toggleSwitchOption[i].classList.toggle('active')
+//
+//     if (influencerOption.classList.contains('active')) {
+//       sectionInfluencers.classList.add('active'),
+//         sectionMovies.classList.remove('active')
+//     } else {
+//       sectionMovies.classList.add('active'),
+//         sectionInfluencers.classList.remove('active')
+//     }
+//   })
+// }
+//
+// const runningLineOne = document.querySelector('.M_RunningLine')
+// const runningLineTwo = document.querySelector('.M_RunningLineTwo')
+//
+// const mediaphone = window.matchMedia(
+//   '(min-width: 768px) and (max-width: 3000px)'
+// )
+//
+// if (mediaphone.matches) {
+//   runningLineOne.innerHTML = runningLineOne.innerHTML + runningLineOne.innerHTML
+//   runningLineTwo.innerHTML = runningLineTwo.innerHTML + runningLineTwo.innerHTML
+// }
