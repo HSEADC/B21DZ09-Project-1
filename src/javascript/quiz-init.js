02c7ace42c367fd0418322b32d2bb138c81e1cd3
Екатерина Никифорова <dayso@192.168.1.4>
2023-06-20 23:46:25 +0300

    Quiz js progress2

--- src/javascript/quiz-init.js
+++ src/javascript/quiz-init.js
@@ -1,17 +1,16 @@
 const quizButton = document.querySelector('.S_Cover .M_ButtonSurvey')
 const quiz = document.querySelector('.S_QuizBlock')
-const tags = document.querySelectorAll('.M_SurveyTag')
-const buttonNext = document.querySelector('.O_QuizBlock .M_ButtonSurvey')
-const buttonBack = document.querySelector('.O_QuizBlock .A_ButtonBack')
+const tags = quiz.querySelectorAll('.M_SurveyTag')
+const buttonNext = quiz.querySelector('.M_ButtonSurvey')
+const buttonBack = quiz.querySelector('.A_ButtonBack')
+const buttonClose = quiz.querySelector('.A_Close')
+const counter = quiz.querySelector('.A_Counter')
+const stage = quiz.querySelectorAll('.A_CircleStage')
+const progressLine = quiz.querySelector('.A_ProgressLine')
 
 function quizInit() {
   quizButton.addEventListener('click', () => {
     quiz.classList.remove('none')
-  })
-}
-function arrayRemove(arr, value) {
-  return arr.filter(function (ele) {
-    return ele != value
   })
 }
 
@@ -27,9 +26,76 @@ function tagSelect() {
   }
 }
 
+function firstQuestionInit() {
+  document.querySelector('.A_QuizText.place').classList.remove('none')
+  document.querySelector('.A_QuizText.mood').classList.add('none')
+  document.querySelector('.A_QuizText.character').classList.add('none')
+
+  document.querySelector('.W_TagBar.place').classList.remove('none')
+  document.querySelector('.W_TagBar.mood').classList.add('none')
+  document.querySelector('.W_TagBar.character').classList.add('none')
+
+  counter.innerHTML = '1/3'
+
+  stage[0].classList.add('current')
+  stage[0].classList.remove('active')
+  stage[1].classList.add('disabled')
+  stage[1].classList.remove('current')
+  stage[2].classList.add('disabled')
+  stage[2].classList.remove('current')
+
+  progressLine.style.width = '0%'
+
+  buttonNext.innerHTML = 'далее <div class="Q_Arrow black"></div>'
+}
+
+function secondQuestionInit() {
+  document.querySelector('.A_QuizText.place').classList.add('none')
+  document.querySelector('.A_QuizText.character').classList.add('none')
+  document.querySelector('.A_QuizText.mood').classList.remove('none')
+
+  document.querySelector('.W_TagBar.place').classList.add('none')
+  document.querySelector('.W_TagBar.character').classList.add('none')
+  document.querySelector('.W_TagBar.mood').classList.remove('none')
+
+  counter.innerHTML = '2/3'
+
+  stage[0].classList.remove('current')
+  stage[0].classList.add('active')
+  stage[1].classList.remove('disabled')
+  stage[1].classList.remove('active')
+  stage[1].classList.add('current')
+  stage[2].classList.add('disabled')
+  stage[2].classList.remove('current')
+
+  progressLine.style.width = '20%'
+
+  buttonNext.innerHTML = 'далее <div class="Q_Arrow black"></div>'
+}
+
+function thirdQuestionInit() {
+  document.querySelector('.A_QuizText.mood').classList.add('none')
+  document.querySelector('.A_QuizText.character').classList.remove('none')
+
+  document.querySelector('.W_TagBar.mood').classList.add('none')
+  document.querySelector('.W_TagBar.character').classList.remove('none')
+
+  counter.innerHTML = '3/3'
+
+  stage[1].classList.remove('current')
+  stage[1].classList.add('active')
+  stage[2].classList.remove('disabled')
+  stage[2].classList.add('current')
+
+  progressLine.style.width = '40%'
+
+  buttonNext.innerHTML = 'завершить <div class="Q_Arrow black"></div>'
+}
+
 function handleNext() {
   let n = 1
   let activePlaces = []
+  let activeMoods = []
 
   buttonNext.addEventListener('click', () => {
     console.log('click')
@@ -37,6 +103,7 @@ function handleNext() {
       n++
     }
 
+    //First question
     if (n == 2) {
       activePlaces = []
       for (let i = 0; i < tags.length; i++) {
@@ -52,15 +119,56 @@ function handleNext() {
         }
       }
       console.log(activePlaces)
+      secondQuestionInit()
+    }
+
+    //Second question
+    if (n == 3) {
+      activeMoods = []
+      for (let i = 0; i < tags.length; i++) {
+        const tag = tags[i]
+        const tagText = tag.innerHTML
+        if (
+          tag.id == 'mood' &&
+          tag.classList.contains('active') &&
+          !activeMoods.includes(tagText)
+        ) {
+          console.log(tagText)
+          activeMoods.push(tagText)
+        }
+      }
+      console.log(activeMoods)
+      thirdQuestionInit()
     }
   })
 
+  //Back
   buttonBack.addEventListener('click', () => {
-    console.log(activePlaces)
     if (n > 1) {
       n--
     }
+
+    if (n == 1) {
+      firstQuestionInit()
+      console.log(activePlaces)
+    }
+
+    if (n == 2) {
+      secondQuestionInit()
+      console.log(activeMoods)
+    }
   })
+  //Close
+  buttonClose.addEventListener('click', () => {
+    n = 1
+    for (let i = 0; i < tags.length; i++) {
+      const tag = tags[i]
+      tag.classList.remove('active')
+    }
+    firstQuestionInit()
+    quiz.classList.add('none')
+  })
+  //
 }
 
 document.addEventListener('DOMContentLoaded', () => {
